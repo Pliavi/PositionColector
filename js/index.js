0bba51c52d203045e4a5fddd7dbc8d20163e190e
Pliavi <pliavi.rpgm@gmail.com>
2017-03-20 01:05:29 -0300

    Little Updates: - Corrected lint - Removed rest spread - Added position by id - Now the position is get after drag, not on it (1 event per drag, rather than "a million")

--- js/index.js
+++ js/index.js
@@ -1,20 +1,22 @@
-let Draggable = require ('Draggable');
-let dragbound = document.getElementById('drag-bound');
-let elements = document.getElementsByClassName('draggable');
+let Draggable = require ('Draggable')
+let dragbound = document.getElementById('drag-bound')
+let elements = document.getElementsByClassName('draggable')
 
 var positions = {
   head: []
 }
 
 let optionsFactory = {
-	limit: dragbound,
-	setCursor: true,
-	onDrag: function (element, x, y) {
-		positions.head = [x, y]
-	}
-};
+  limit: dragbound,  
+  setCursor: true,
+  onDragEnd: function (el, x, y) {
+    positions[el.id] = [x, y]
+    console.log(positions)
+  }
+}
 
-[...elements].forEach((element) => {
-  let options = optionsFactory;
-  new Draggable (element, options);
-});
+for(let i = 0; i < elements.length; i++){
+  let element = elements[i]
+  let options = optionsFactory
+  new Draggable (element, options)
+}
