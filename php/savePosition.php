24f319dea481d46282b4573d371721776ed708e4
Pliavi <pliavi.rpgm@gmail.com>
2017-03-26 12:56:51 -0300

    Testing, Position class now working

--- php/savePosition.php
+++ php/savePosition.php
@@ -1,3 +1,15 @@
 <?php
-$data = $_POST['data'];
+include 'Config.php';
+include 'Position.php';
+
+// $data = $_POST['data'];
+
+$data = [
+  'folder' => 1,
+  'index' => 1,
+  'positions' => [
+    'lhand' => [256, 256]
+  ]
+];
+
 new Position($data);
