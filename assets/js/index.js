f01e5d9f5c502ea1a604e1778f2a638a9133a6de
Pliavi <pliavi.rpgm@gmail.com>
2017-04-06 11:13:22 -0300

    changed global vars to Store

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,9 @@
+var Store = require('./store')
 let Draggable = require ('Draggable')
 var Ajax = require('./ajax')
 
 let dragbound = document.getElementById('drag-bound')
 let elements = document.getElementsByClassName('draggable')
-let draggables = {};
-var positions = {}
 
 let optionsFactory = {
   limit: dragbound,  
@@ -13,41 +12,41 @@ let optionsFactory = {
   },
   onDragEnd: function (el, x, y) {
     el.style.cursor = 'initial'
-    positions[el.id] = [x, y]
-    console.log(positions)
+    Store.positions[el.id] = [x, y]
+    console.log(Store.positions)
   }
 }
 
 for(let i = 0; i < elements.length; i++){
   let element = elements[i]
   let options = optionsFactory
-  draggables[element.id] = new Draggable (element, options)
+  Store.draggables[element.id] = new Draggable (element, options)
 }
 
-Ajax.getActual()
+Ajax.getImage('actual')
 let imageSize = dragbound.clientWidth
 let p = (x) => imageSize*x/100
 
-draggables['head']           .set(p(50), p(10))
-draggables['neck']           .set(p(50), p(18))
-draggables['shoulder_left']  .set(p(56), p(20))
-draggables['shoulder_right'] .set(p(44), p(20))
-draggables['elbow_left']     .set(p(62), p(32.5))
-draggables['elbow_right']    .set(p(38), p(32.5))
-draggables['hand_left']      .set(p(60), p(46))
-draggables['hand_right']     .set(p(40), p(46))
-draggables['torso']          .set(p(50), p(35))
-draggables['hip_left']       .set(p(53), p(40))
-draggables['hip_right']      .set(p(47), p(40))
-draggables['knee_left']      .set(p(55), p(55))
-draggables['knee_right']     .set(p(45), p(55))
-draggables['foot_left']      .set(p(55), p(68))
-draggables['foot_right']     .set(p(45), p(68))
+Store.draggables['head']           .set(p(50), p(10))
+Store.draggables['neck']           .set(p(50), p(18))
+Store.draggables['shoulder_left']  .set(p(56), p(20))
+Store.draggables['shoulder_right'] .set(p(44), p(20))
+Store.draggables['elbow_left']     .set(p(62), p(32.5))
+Store.draggables['elbow_right']    .set(p(38), p(32.5))
+Store.draggables['hand_left']      .set(p(60), p(46))
+Store.draggables['hand_right']     .set(p(40), p(46))
+Store.draggables['torso']          .set(p(50), p(35))
+Store.draggables['hip_left']       .set(p(53), p(40))
+Store.draggables['hip_right']      .set(p(47), p(40))
+Store.draggables['knee_left']      .set(p(55), p(55))
+Store.draggables['knee_right']     .set(p(45), p(55))
+Store.draggables['foot_left']      .set(p(55), p(68))
+Store.draggables['foot_right']     .set(p(45), p(68))
 
 for(let i = 0; i < elements.length; i++){
   let element = elements[i]  
-  positions[element.id] = draggables[element.id].get();
+  Store.positions[element.id] = Store.draggables[element.id].get()
 }
-console.log(positions)
 
-document.getElementById('next-button').addEventListener('click', () => Ajax.sendData(positions));
+document.getElementById('next-button').addEventListener('click', () => Ajax.sendData(Store.positions, 'next'))
+document.getElementById('last-button').addEventListener('click', () => Ajax.sendData(Store.positions, 'last'))
